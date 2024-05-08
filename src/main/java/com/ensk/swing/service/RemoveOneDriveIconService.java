2a5bb2db4ad0c4bfa0220c2e6d3dcbb49bde476f
魏大义 <tojohnonly@gmail.com>
2024-05-08 16:21:20 +0800

    Update RemoveOneDriveIconService.java

--- src/main/java/com/ensk/swing/service/RemoveOneDriveIconService.java
+++ src/main/java/com/ensk/swing/service/RemoveOneDriveIconService.java
@@ -6,8 +6,7 @@ public class RemoveOneDriveIconService {
 
     public static void removeOneDriveIcon() {
         try {
-            Runtime.getRuntime().exec("cmd /c reg add HKEY_CLASSES_ROOT\\CLSID\\{018D5C66-4533-4307-9B53-224DE2ED1FE6} "
-                + "/v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0 /f");
+            Runtime.getRuntime().exec("cmd /c reg add HKEY_CLASSES_ROOT\\CLSID\\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0 /f");
             Thread.sleep(1000);
         } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
